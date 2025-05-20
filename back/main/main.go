fb7119002f6065ef0ccf1642db0ec7cf8922d58c
corentin-cpp <cocosdev615@gmail.com>
2025-05-20 09:41:24 +0200

    feat

--- back/main/main.go
+++ back/main/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	templatePath, err := filepath.Abs(filepath.Join("..", "template/html"))
+	templatePath, err := filepath.Abs(filepath.Join("..", "..", "template/html"))
 	if err != nil {
 		log.Fatal(err)
 	}
