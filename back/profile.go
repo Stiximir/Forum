5c7b335b3dd0790a849dbdaeb57791b6df11d879
Orlex69 <alexis.brad@ynov.com>
2025-06-04 16:55:40 +0200

    ProfilePicture

--- back/profile.go
+++ back/profile.go
@@ -9,10 +9,11 @@ import (
 )
 
 type User struct {
-	ID       int
-	Username string
-	Email    string
-	Password string
+	ID             int
+	Username       string
+	Email          string
+	Password       string
+	ProfilePicture string
 }
 
 type Likes struct {
@@ -36,14 +37,14 @@ func Delete(db *sql.DB, id string) (int64, error) {
 
 func GetRecentSearches(db *sql.DB) []User {
 	var searches []User
-	row, err := db.Query("SELECT id, username, email, password FROM users WHERE id = ?", UserP)
+	row, err := db.Query("SELECT id, username, email, password, profile_picture FROM users WHERE id = ?", UserP)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer row.Close()
 	for row.Next() { // Iterate and fetch the records from result cursor
 		item := User{}
-		err := row.Scan(&item.ID, &item.Username, &item.Email, &item.Password)
+		err := row.Scan(&item.ID, &item.Username, &item.Email, &item.Password, &item.ProfilePicture)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -98,6 +99,10 @@ func DBprofile(db *sql.DB, nil error) {
 		data.ID = strconv.Itoa(item.ID)
 		data.Username = item.Username
 		data.Email = item.Email
+		data.ProfilePicture = item.ProfilePicture
+		if item.ProfilePicture == "" {
+			data.ProfilePicture = "default.png" // Default profile picture if none is set
+		}
 	}
 	for _, item := range searchesLikes {
 		fmt.Printf("Likes: %d\n", item.Likes)
@@ -110,14 +115,15 @@ func DBprofile(db *sql.DB, nil error) {
 }
 
 type Data struct {
-	ID       string
-	Username string
-	Email    string
-	User     string
-	Likes    int
-	Posts    int
-	UserP    string
-	RealUser string
+	ID             string
+	Username       string
+	Email          string
+	User           string
+	Likes          int
+	Posts          int
+	UserP          string
+	RealUser       string
+	ProfilePicture string
 }
 
 var data = Data{}
