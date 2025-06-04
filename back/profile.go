6966df0b381ba537afeee223de0af88b645a67d9
Stiximir <84101101+Stiximir@users.noreply.github.com>
2025-06-04 16:00:12 +0200

    UPDATE PROFIL

--- back/profile.go
+++ back/profile.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type User struct {
@@ -12,6 +13,14 @@ type User struct {
 	Username string
 	Email    string
 	Password string
+}
+
+type Likes struct {
+	Likes int
+}
+
+type Posts struct {
+	Posts int
 }
 
 var UserP string = "2"
@@ -43,22 +52,72 @@ func GetRecentSearches(db *sql.DB) []User {
 	return searches
 }
 
+func GetLikes(db *sql.DB) []Likes {
+	var searches []Likes
+	row, err := db.Query("SELECT COUNT(*) FROM likes WHERE user_id = ?", UserP)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer row.Close()
+	for row.Next() { // Iterate and fetch the records from result cursor
+		item := Likes{}
+		err := row.Scan(&item.Likes)
+		if err != nil {
+			log.Fatal(err)
+		}
+		searches = append(searches, item)
+	}
+	return searches
+}
+
+func GetPosts(db *sql.DB) []Posts {
+	var searches []Posts
+	row, err := db.Query("SELECT COUNT(*) FROM posts WHERE user_id = ?", UserP)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer row.Close()
+	for row.Next() { // Iterate and fetch the records from result cursor
+		item := Posts{}
+		err := row.Scan(&item.Posts)
+		if err != nil {
+			log.Fatal(err)
+		}
+		searches = append(searches, item)
+	}
+	return searches
+}
+
 func DBprofile(db *sql.DB, nil error) {
 	searches := GetRecentSearches(db)
+	searchesLikes := GetLikes(db)
+	searchesPosts := GetPosts(db)
 	fmt.Printf("ID\tUsername\tEmail\n")
 	for _, item := range searches {
 		fmt.Printf("%d\t%s\t%s\n", item.ID, item.Username, item.Email)
-		data.ID = item.ID
+		data.ID = strconv.Itoa(item.ID)
 		data.Username = item.Username
 		data.Email = item.Email
+	}
+	for _, item := range searchesLikes {
+		fmt.Printf("Likes: %d\n", item.Likes)
+		data.Likes = item.Likes
+	}
+	for _, item := range searchesPosts {
+		fmt.Printf("Posts: %d\n", item.Posts)
+		data.Posts = item.Posts
 	}
 }
 
 type Data struct {
-	ID       int
+	ID       string
 	Username string
 	Email    string
 	User     string
+	Likes    int
+	Posts    int
+	UserP    string
+	RealUser string
 }
 
 var data = Data{}
@@ -70,6 +129,9 @@ func Profile(w http.ResponseWriter, r *http.Request, templatePath string) {
 	defer db.Close()
 
 	UserP = r.URL.Query().Get("userId")
+	RealUser := GetCookie(r, "user").Cookie
+	data.RealUser = RealUser
+	data.UserP = UserP
 
 	if r.Method == http.MethodPost {
 		_, err = Delete(db, UserP)
